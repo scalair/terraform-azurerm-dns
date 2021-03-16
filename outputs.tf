ede29452258868986f37bff7ba3546c027280d0d
Xat59 <descamps.jeremy@gmail.com>
2021-03-16 16:21:16 +0100

    add: zone and A records outputs

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,24 @@
 output "zone_id" {
-  description = "The DNS Zone ID."
-  value       = var.create_dns_zone ? (var.public_dns_zone ? azurerm_dns_zone.zone_public.0.id : azurerm_private_dns_zone.zone_private.0.id) : null
+  description   = "The DNS Zone ID."
+  value         = var.create_dns_zone ? (var.public_dns_zone ? azurerm_dns_zone.zone_public.0.id : azurerm_private_dns_zone.zone_private.0.id) : null
 }
 
 output "zone_max_number_of_record_sets" {
-  description = "Maximum number of Records in the zone."
-  value       = var.create_dns_zone ? (var.public_dns_zone ? azurerm_dns_zone.zone_public.0.max_number_of_record_sets : azurerm_private_dns_zone.zone_private.0.max_number_of_record_sets) : null
+  description   = "Maximum number of Records in the zone."
+  value         = var.create_dns_zone ? (var.public_dns_zone ? azurerm_dns_zone.zone_public.0.max_number_of_record_sets : azurerm_private_dns_zone.zone_private.0.max_number_of_record_sets) : null
 }
 
 output "zone_number_of_record_sets" {
-  description = "The number of records already in the zone."
-  value       = var.create_dns_zone ? (var.public_dns_zone ? azurerm_dns_zone.zone_public.0.number_of_record_sets : azurerm_private_dns_zone.zone_private.0.number_of_record_sets) : null
+  description   = "The number of records already in the zone."
+  value         = var.create_dns_zone ? (var.public_dns_zone ? azurerm_dns_zone.zone_public.0.number_of_record_sets : azurerm_private_dns_zone.zone_private.0.number_of_record_sets) : null
+}
+
+output "a_record_id" {
+  description   = "A list of DNS A Record ID."
+  value         =   [ var.create_dns_zone ? (var.public_dns_zone ? azurerm_dns_a_record.a_record_public_with_zone.*.id : azurerm_private_dns_a_record.a_record_private_with_zone.*.id) : (var.public_dns_zone ? azurerm_dns_a_record.a_record_public_no_zone.*.id : azurerm_private_dns_a_record.a_record_private_no_zone.*.id)]
+}
+
+output "a_record_fqdn" {
+  description   = "A list of DNS A Record FQDN."
+  value         =   [ var.create_dns_zone ? (var.public_dns_zone ? azurerm_dns_a_record.a_record_public_with_zone.*.fqdn : azurerm_private_dns_a_record.a_record_private_with_zone.*.fqdn) : (var.public_dns_zone ? azurerm_dns_a_record.a_record_public_no_zone.*.fqdn : azurerm_private_dns_a_record.a_record_private_no_zone.*.fqdn)]
 }
